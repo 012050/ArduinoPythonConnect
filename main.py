df8c6b57a6451ecc502115635943620f8c1efc0a
012050 <beomjunchoe@naver.com>
2023-04-26 14:46:05 +0900

    장치 자동 연결

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
-import serial
-import time
+import serial.tools.list_ports
 import os
+import time
 import pyautogui
 
-# port.txt 파일에 저장된 포트 번호를 읽어옴
-try:
-    f = open(os.getcwd() + "\port.txt")
-    port_data = f.read()
-    f.close()
-except:
-    print("port.txt 파일이 없습니다.")
-    pyautogui.alert("port.txt 파일이 없습니다.")
-    port_data = input("포트 번호를 입력해 주세요: ")
-    f = open(os.getcwd() + "\port.txt", 'w')
-    f.write(port_data)
-    f.close()
+device = serial.tools.list_ports.comports()
+for port, desc, hwid in sorted(device):
+    # print(f"port: {port}: desc: {desc} hwid: [{hwid}]")
+    print(port)
+    desc = desc.split()[1].split('(')[0]
+    print(desc)
+    print(hwid)
+    if desc == "Leonardo":
+        print("아두이노 연결 성공")
+        port_data = port
 
 try:
     py_serial = serial.Serial(
         # Window
-        port=f'COM{port_data}',
+        # port=f'COM{port_data}',
+        port=port_data,
 
         # Linux
         # port=f'/dev/ttyACM{port_data}',
@@ -40,7 +39,9 @@ while True:
         print(a_command)
     
     c_commend = input('아두이노에게 내릴 명령:')
-    if c_commend == "exit":
+    if c_commend == '':
+        c_commend = 'a'
+    elif c_commend == "exit":
         print("시리얼 통신 종료")
         break
     py_serial.write(c_commend.encode())
